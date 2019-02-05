98561d552123bc4790b8dd7c9b3533b5f862f28b
David Varga <varga.david@duvinet.hu>
2019-02-05 10:36:08 +0100

    add more debugging options to compiling

--- _compile.sh
+++ _compile.sh
@@ -1,3 +1,3 @@
-gcc -D_GNU_SOURCE -c *.c -ggdb3 -Wall -Wextra -Wno-format-zero-length `mysql_config --cflags --libs`
-gcc *.o -lm -lpthread -o listen -ggdb3 -Wall -Wextra `mysql_config --cflags --libs`
+gcc -D_GNU_SOURCE -c *.c -g3 -ggdb3 -Wall -Wextra -Wno-format-zero-length `mysql_config --cflags --libs`
+gcc *.o -lm -lpthread -o listen -g3 -ggdb3 -Wall -Wextra `mysql_config --cflags --libs`
 rm *.o
