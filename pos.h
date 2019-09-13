073a43de268bf35a247fbc9a430b1989b04ca897
David Varga <varga.david@duvinet.hu>
2019-09-13 19:14:41 +0200

    remove unexisting function declarations get_pos and sort_pos

--- pos.h
+++ pos.h
@@ -4,10 +4,6 @@
 
 int del_pos(char adat[MAXBUFLEN]);			//Terkeppont torlese
 
-void get_pos(void);					//Terkeppontok listazasa
-
-void sort_pos(void);					//Poziciok sorba rendezese
-
 void clear_map(void);					//Terkep torlese
 
 int save_map(char adat[MAXBUFLEN]);			//Terkep mentese
